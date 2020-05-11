9e8ab25e1cc55d7c5929406abebb737dbec30788
shuto <shuto4192@gmail.com>
2020-05-12 01:11:58 +0900

    汎用化

--- routers/items.py
+++ routers/items.py
@@ -3,27 +3,34 @@ from fastapi import APIRouter, HTTPException
 from tortoise.contrib.fastapi import HTTPNotFoundError
 from starlette.status import HTTP_204_NO_CONTENT
 
+from utiles.response import HTTP_404_NOT_FOUND
 from models.items import Item_Pydantic, ItemIn_Pydantic, Items
 
 
+end_point = "items"
+tags=["items"]
+model = Items
+model_pydanic = Item_Pydantic
+model_in_pydanic = ItemIn_Pydantic
+
 router = APIRouter()
 
-@router.get("/items", tags=["items"], response_model=List[Item_Pydantic])
-async def list_items():
-    return await Item_Pydantic.from_queryset(Items.all())
+@router.get("/{end_point}", tags=tags, response_model=List[model_pydanic])
+async def get_list():
+    return await model_pydanic.from_queryset(model.all())
 
-@router.post("/items", tags=["items"], response_model=Item_Pydantic)
-async def create_item(item: ItemIn_Pydantic):
-    item_obj = await Items.create(**item.dict(exclude_unset=True))
-    return await Item_Pydantic.from_tortoise_orm(item_obj)
+@router.post("/{end_point}", tags=tags, response_model=model_pydanic)
+async def create(data: model_in_pydanic):
+    obj = await model.create(**data.dict(exclude_unset=True))
+    return await model_pydanic.from_tortoise_orm(obj)
 
-@router.patch("/items/{item_id}", tags=["items"], response_model=Item_Pydantic, responses={404: {"model": HTTPNotFoundError}})
-async def update_item(item_id: int, item: ItemIn_Pydantic):
-    await Items.filter(id=item_id).update(**item.dict(exclude_unset=True))
-    return await Item_Pydantic.from_queryset_single(Items.get(id=item_id))
+@router.patch("/{end_point}/{id}", tags=tags, response_model=model_pydanic, responses=HTTP_404_NOT_FOUND)
+async def update(id: int, data: model_in_pydanic):
+    await model.filter(id=id).update(**data.dict(exclude_unset=True))
+    return await model_pydanic.from_queryset_single(model.get(id=id))
 
-@router.delete("/items/{item_id}", tags=["items"], status_code=HTTP_204_NO_CONTENT)
-async def delete_item(item_id: int):
-    deleted_count = await Items.filter(id=item_id).delete()
+@router.delete("/{end_point}/{id}", tags=tags, status_code=HTTP_204_NO_CONTENT)
+async def delete(id: int):
+    deleted_count = await model.filter(id=id).delete()
     if not deleted_count:
         raise HTTPException(status_code=404, detail=f"Not found")
